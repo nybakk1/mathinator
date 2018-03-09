275bf7519db77cc18b38b445f3fea31aa4d9c074
JoaQuin <joakim.sather@hotmail.com>
2018-03-09 12:08:44 +0100

    Added some commments.

--- structmatrix.py
+++ structmatrix.py
@@ -8,17 +8,17 @@ from scipy.sparse.linalg import spsolve
 def lagA(n):
     e = sp.ones(n)
     A = spdiags([e, -4*e, 6*e, -4*e, e], [-2, -1, 0, 1, 2], n, n)
-    A = lil_matrix(A)  # TODO: Spør Rivertz om vi kan hardkode her.
-    B = csr_matrix([[16, -9, 8/3, -1/4],
+    A = lil_matrix(A)
+    B = csr_matrix([[16, -9, 8/3, -1/4],  # TODO: Spør Rivertz om vi kan hardkode her.
                     [16/17, -60/17, 72/17, -28/17],
-                    [-12/17, 96/17, 156/17, 72/17]
+                    [-12/17, 96/17, -156/17, 72/17]
                     ])
 
     A[0, 0:4] = B[0, :]
     A[n-2, n-4:n] = B[1, :]
     A[n-1, n-4:n] = B[2, :]
 
-    return A
+    return A.tocsr()
 
 
 L = 2.00  # Length of the beam
@@ -30,9 +30,8 @@ w = 0.30  # Width
 d = 0.03  # Thickness
 E = 1.3 * (10 ** 10)  # Young’s modulus of the wood
 I = (w * (d ** 3))/12  # The area moment of inertia I around the center of mass of a beam
-g = -9.81  # Gravitational force
+g = 9.81  # Gravitational force
 weight = -480 * w * d * g  # Weight of a slice of the diving board
 b = [(weight * h4)/(E * I)]*n  # The b-vector
-
 y = spsolve(A, b)  # Solving the matrix
 print(y)
