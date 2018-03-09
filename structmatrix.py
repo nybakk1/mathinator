543ec18156a16c75806664269e75f44218e0d9b1
JoaQuin <joakim.sather@hotmail.com>
2018-03-09 11:13:37 +0100

    Added some commments.

--- structmatrix.py
+++ structmatrix.py
@@ -21,18 +21,18 @@ def lagA(n):
     return A
 
 
-L = 2.00
-n = 10
-A = lagA(n)
-h = L / n
+L = 2.00  # Length of the beam
+n = 10  # Number of segments
+A = lagA(n)  # Creating the A matrix
+h = L / n  # Length of each segment
 h4 = h ** 4
-w = 0.30
-d = 0.03
-E = 1.3 * (10 ** 10)
-I = (w * (d ** 3))/12
-g = -9.81
-weight = -480 * w * d * g
-b = [(weight * h4)/(E * I)]*n
+w = 0.30  # Width
+d = 0.03  # Thickness
+E = 1.3 * (10 ** 10)  # Young’s modulus of the wood
+I = (w * (d ** 3))/12  # The area moment of inertia I around the center of mass of a beam
+g = -9.81  # Gravitational force
+weight = -480 * w * d * g  # Weight of a slice of the diving board
+b = [(weight * h4)/(E * I)]*n  # The b-vector
 
-y = spsolve(A, b)
+y = spsolve(A, b)  # Solving the matrix
 print(y)
