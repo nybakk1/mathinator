41982978e54460f0bf67e7fd793157f51aaa9199
JoaQuin <joakim.sather@hotmail.com>
2018-03-09 11:10:03 +0100

    First attempt at exercise 3.

--- structmatrix.py
+++ structmatrix.py
@@ -2,11 +2,12 @@ import scipy as sp
 from scipy.sparse import spdiags
 from scipy.sparse import lil_matrix
 from scipy.sparse import csr_matrix
+from scipy.sparse.linalg import spsolve
 
 
 def lagA(n):
     e = sp.ones(n)
-    A = (spdiags([e, -4*e, 6*e, -4*e, e], [-2, -1, 0, 1, 2], n, n)).toarray()
+    A = spdiags([e, -4*e, 6*e, -4*e, e], [-2, -1, 0, 1, 2], n, n)
     A = lil_matrix(A)  # TODO: Spør Rivertz om vi kan hardkode her.
     B = csr_matrix([[16, -9, 8/3, -1/4],
                     [16/17, -60/17, 72/17, -28/17],
@@ -17,7 +18,21 @@ def lagA(n):
     A[n-2, n-4:n] = B[1, :]
     A[n-1, n-4:n] = B[2, :]
 
-    return A.toarray()
+    return A
 
 
-print(lagA(5))
+L = 2.00
+n = 10
+A = lagA(n)
+h = L / n
+h4 = h ** 4
+w = 0.30
+d = 0.03
+E = 1.3 * (10 ** 10)
+I = (w * (d ** 3))/12
+g = -9.81
+weight = -480 * w * d * g
+b = [(weight * h4)/(E * I)]*n
+
+y = spsolve(A, b)
+print(y)
