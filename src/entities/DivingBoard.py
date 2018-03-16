ec1c9ca5520502c7c2f97b71fbef64aa541ca0c8
Daniel Klock <daniel.a.klock@gmail.com>
2018-03-16 14:12:38 +0100

    Update pydocs.

--- src/entities/DivingBoard.py
+++ src/entities/DivingBoard.py
@@ -6,7 +6,15 @@ class DivingBoard:
     E = 1.3 * (10 ** 10)
 
     def __init__(self, length, width, thickness):
+        """
+        Construct a diving board object.
+        :param length: the length of the board in meters.
+        :param width: the width of the board in meters.
+        :param thickness: the thickness of the board in meters
+        """
         self.length = length
         self.width = width
         self.thickness = thickness
+
+        """Area moment of inertia around the center of mass of a beam"""
         self.I = (self.width * (self.thickness ** 3)) / 12
