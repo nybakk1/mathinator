1866b97d7a5fc19122323d6b24a486b519f3c3b0
JoaQuin <joakim.sather@hotmail.com>
2018-03-18 21:27:15 +0100

    Fixed force n shit

--- src/structmatrix.py
+++ src/structmatrix.py
@@ -42,7 +42,7 @@ divingBoard = DivingBoard(2.00, 0.30, 0.03)
 A = create_matrix(segments)     # Creating the A matrix
 segm_len = divingBoard.length / segments    # Length of each segment
 h4 = segm_len ** 4
-force = (-480 * divingBoard.width * divingBoard.thickness * g * divingBoard.length) / segments  # Force acting on a slice of the diving board
+force = (-480 * divingBoard.width * divingBoard.thickness * g) # Force acting on a slice of the diving board
 b1 = [(force * h4) / (divingBoard.E * divingBoard.I)] * segments  # The b-vector
 y = spsolve(A, b1)  # Solving the matrix
 
@@ -62,16 +62,37 @@ for i in range(0, segments):
 # print("Cond(A): ", norm(A) * norm(inv(A)))
 # print("Foroverfeil (4e): ", np.linalg.norm(R - y, 1)/2**-52, "maskinepsilon")
 
+# cond5 = sp.ones(11)
+# X5 = sp.ones(11)
+# Y5 = sp.ones(11)
+# Z5 = sp.ones(11)
+# H5 = sp.ones(11)
+# R5 = sp.ones(11)
+
 for i in range(1, 12):
     segments = 10 * 2 ** i
+    # X5[i-1] = segments
     segm_len = divingBoard.length / segments  # Length of each segment
     h4 = segm_len ** 4
     A = create_matrix(segments)
-    # cond = norm(A) * norm(inv(A))
+    if i < 9:
+        cond = norm(A) * norm(inv(A))
+    else:
+        cond = 0
     b = [(force * h4)/(divingBoard.E * divingBoard.I)] * segments
     y = spsolve(A, b)
+    # cond5[i-1] = cond * (2 ** -52)
+    # R5[i - 1] = ((force * 4) / (24 * divingBoard.E * divingBoard.I)) * (4 - 16 + 24)
+    # Y5[i-1] = y[-1]
+    # Z5[i - 1] = abs(y[-1] - R5[i - 1])
+    # H5[i-1] = 4/(segments**2)
     # print("Cond(A)", cond)
     # print(abs(y[-1] - R[-1]))
+
+# pl.plot(np.log(X5), np.log(Z5))  # Oppgave 6c and 6d
+# pl.plot(np.log(X5), np.log(cond5)) # Oppgave 6d
+# pl.plot(np.log(X5), np.log(H5))  # Oppgave 6d
+# pl.show()
 
 X = sp.ones(11)
 Y = sp.ones(11)
@@ -100,10 +121,40 @@ for i in range(1, 12):
     Z[i-1] = abs(y[-1] - R1[i-1])
     Y[i-1] = (y[-1])
     H2[i-1] = 4/(segments**2)
-
 # pl.plot(X, Y)  # Oppgave 6b
 # pl.plot(X, R1)  # Oppgave 6b
-pl.plot(np.log(X), np.log(Z))  # Oppgave 6c
-pl.plot(np.log(X), np.log(Cond))
-pl.plot(np.log(X), np.log(H2))
+pl.plot(np.log(X), np.log(Z))  # Oppgave 6c and 6d
+pl.plot(np.log(X), np.log(Cond)) # Oppgave 6d
+pl.plot(np.log(X), np.log(H2))  # Oppgave 6d
 pl.show()
+
+
+for i in range(1, 2):
+    segments = 10000
+    segm_len = divingBoard.length / segments
+    h4 = segm_len ** 4
+    A = create_matrix(segments)
+    g = 9.81
+    b = sp.ones(segments)
+    segm_start = 0
+    segm_stop = segm_len
+    TOL = 0.00001
+    f_pers = (g * 50) / 0.3
+    for j in range(0, segments):
+        if segm_stop > 1.70:
+            if segm_start > 1.70:
+                b[j] = ((force - f_pers) * h4) / (divingBoard.E * divingBoard.I)
+            else:
+                b[j] = ((force - (f_pers/(segm_stop - 1.7)) * h4) / (divingBoard.E * divingBoard.I))
+        else:
+            b[j] = ((force * h4) / (divingBoard.E * divingBoard.I))
+
+        # if segm_stop < 1.70 + TOL and segm_stop > 1.70 - TOL:
+        if 1.70 - TOL < segm_stop < 1.70 + TOL:
+            b[j] = ((force * h4) / (divingBoard.E * divingBoard.I))
+        if 1.70 - TOL < segm_start < 1.70 + TOL:
+            b[j] = ((force - f_pers) * h4) / (divingBoard.E * divingBoard.I)
+        segm_start += segm_len
+        segm_stop += segm_len
+    y = spsolve(A, b)
+    # print(y[-1])
